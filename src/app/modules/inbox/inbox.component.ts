c19d8233838ccec0a8aa7a77edf8aa73b945a404
Matheus-Hespagnola <matheusah@ciandt.com>
2024-08-20 17:04:39 -0300

    alteracoes inbox

--- src/app/modules/inbox/inbox.component.ts
+++ src/app/modules/inbox/inbox.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgToastService } from 'ng-angular-popup';
 import { environment } from 'src/environments/environment';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-inbox',
@@ -12,50 +14,73 @@ export class InboxComponent implements OnInit {
   documents: any[] = [];
   recados: any[] = [];
   loading: boolean = false;
+  selectedDocument: any;
+  selectedType: string = '';
+  newRegistrantEmail: string = sessionStorage.getItem('userEmail') || '';
+  recipientEmail: string = '';
+  emails: string[] = [];
 
-  constructor(private http: HttpClient, private toast: NgToastService) {}
+  @ViewChild('forwardModal') forwardModal: TemplateRef<any> | undefined;
+
+  constructor(
+    private http: HttpClient, 
+    private toast: NgToastService, 
+    private modalService: NgbModal,
+    private apiServiceService: ApiServiceService
+  ) {}
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.loadDocuments();
     this.loadRecados();
+    this.loadNomePessoas();
   }
 
   loadDocuments() {
     this.loading = true;
     const recipientEmail = sessionStorage.getItem('userEmail');
-    this.http.get(`${environment.apiUrl}/documentos/${recipientEmail}`).subscribe(
-      (response: any) => {
-        this.documents = response.documentos.sort((a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-        this.loading = false;
-      },
-      (error) => {
-        this.toast.error({ detail: 'ERROR', summary: 'Erro ao carregar documentos.' });
-        this.loading = false;
-      }
-    );
+    if (recipientEmail) {
+      this.http.get(`${environment.apiUrl}/documentos/${recipientEmail}`).subscribe(
+        (response: any) => {
+          this.documents = response.documentos.sort((a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+          this.loading = false;
+        },
+        (error) => {
+          this.toast.error({ detail: 'ERROR', summary: 'Erro ao carregar documentos.' });
+          this.loading = false;
+        }
+      );
+    } else {
+      this.toast.error({ detail: 'ERROR', summary: 'Email do destinatário não encontrado.' });
+      this.loading = false;
+    }
   }
 
   loadRecados() {
     this.loading = true;
     const recipientEmail = sessionStorage.getItem('userEmail');
-    this.http.get(`${environment.apiUrl}/recados/${recipientEmail}`).subscribe(
-      (response: any) => {
-        this.recados = response.recados;
-        this.loading = false;
-      },
-      (error) => {
-        this.toast.error({ detail: 'ERROR', summary: 'Erro ao carregar recados.' });
-        this.loading = false;
-      }
-    );
+    if (recipientEmail) {
+      this.http.get(`${environment.apiUrl}/recados/${recipientEmail}`).subscribe(
+        (response: any) => {
+          this.recados = response.recados;
+          this.loading = false;
+        },
+        (error) => {
+          this.toast.error({ detail: 'ERROR', summary: 'Erro ao carregar recados.' });
+          this.loading = false;
+        }
+      );
+    } else {
+      this.toast.error({ detail: 'ERROR', summary: 'Email do destinatário não encontrado.' });
+      this.loading = false;
+    }
   }
 
   markDocumentAsRead(documentId: string) {
     this.http.patch(`${environment.apiUrl}/documentos/${documentId}/read`, {}).subscribe(
       (response: any) => {
         this.toast.success({ detail: 'SUCCESS', summary: 'Documento marcado como lido.' });
-        this.loadDocuments(); // Recarregar os documentos após marcar como lido
+        this.loadDocuments();
       },
       (error) => {
         this.toast.error({ detail: 'ERROR', summary: 'Erro ao marcar documento como lido.' });
@@ -67,11 +92,82 @@ export class InboxComponent implements OnInit {
     this.http.patch(`${environment.apiUrl}/recados/${recadoId}/read`, {}).subscribe(
       (response: any) => {
         this.toast.success({ detail: 'SUCCESS', summary: 'Recado marcado como lido.' });
-        this.loadRecados(); // Recarregar os recados após marcar como lido
+        this.loadRecados();
       },
       (error) => {
         this.toast.error({ detail: 'ERROR', summary: 'Erro ao marcar recado como lido.' });
       }
     );
   }
+
+  openForwardModal(item: any, type: string) {
+    this.selectedDocument = item;
+    this.selectedType = type;
+    this.modalService.open(this.forwardModal);
+  }
+
+  forwardItem(newRegistrantEmail: string, recipientEmail: string) {
+    if (this.selectedType === 'document') {
+      this.http.post(`${environment.apiUrl}/documentos/forward`, { documentId: this.selectedDocument._id, newRegistrant: newRegistrantEmail, newRecipient: recipientEmail }).subscribe(
+        (response: any) => {
+          this.toast.success({ detail: 'SUCCESS', summary: 'Documento encaminhado com sucesso.' });
+          this.loadDocuments();
+        },
+        (error) => {
+          this.toast.error({ detail: 'ERROR', summary: 'Erro ao encaminhar documento.' });
+        }
+      );
+    } else if (this.selectedType === 'recado') {
+      this.http.post(`${environment.apiUrl}/recados/forward`, { recadoId: this.selectedDocument._id, newRegistrant: newRegistrantEmail, newRecipient: recipientEmail }).subscribe(
+        (response: any) => {
+          this.toast.success({ detail: 'SUCCESS', summary: 'Recado encaminhado com sucesso.' });
+          console.log(recipientEmail);
+          this.loadRecados();
+        },
+        (error) => {
+          this.toast.error({ detail: 'ERROR', summary: 'Erro ao encaminhar recado.' });
+        }
+      );
+    }
+  }
+
+  updateDocumentStatus(documentId: string, status: string) {
+    const updatedBy = sessionStorage.getItem('userEmail');
+    this.http.patch(`${environment.apiUrl}/documentos/${documentId}/status`, { status, updatedBy }).subscribe(
+      (response: any) => {
+        this.toast.success({ detail: 'SUCCESS', summary: 'Status do documento atualizado.' });
+        this.loadDocuments();
+      },
+      (error) => {
+        this.toast.error({ detail: 'ERROR', summary: 'Erro ao atualizar status do documento.' });
+      }
+    );
+  }
+
+  updateRecadoStatus(recadoId: string, status: string) {
+    const updatedBy = sessionStorage.getItem('userEmail');
+    this.http.patch(`${environment.apiUrl}/recados/${recadoId}/status`, { status, updatedBy }).subscribe(
+      (response: any) => {
+        this.toast.success({ detail: 'SUCCESS', summary: 'Status do recado atualizado.' });
+        this.loadRecados();
+      },
+      (error) => {
+        this.toast.error({ detail: 'ERROR', summary: 'Erro ao atualizar status do recado.' });
+      }
+    );
+  }
+
+  loadNomePessoas(): void {
+    this.loading = true;
+    this.apiServiceService.loadNomePessoas().subscribe(
+      (pessoasNames: { email: string }[]) => {
+        this.emails = pessoasNames.map(pessoa => pessoa.email);
+        this.loading = false;
+      },
+      error => {
+        this.loading = false;
+      }
+    );
+  }
+
 }
